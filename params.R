f36b5ed4f0bb2bf6ac2889bcf2a5980cfd0abfb7
aghaeeyan <a.aghaeeyan@gmail.com>
2025-11-26 18:50:30 -0500

    undoing some changes

--- params.R
+++ params.R
@@ -14,5 +14,11 @@ t0 <-0
 # cut-off times according to which cohorts are selected
 cutoffTime <- seq(from=0.1, to = 2.5, by=0.1)
 
+xpos <- 0.85; ypos <- 0.25
+stdvShape <- 19; muRcShape <-8
+kwithShape <- 0; kbetShape <- 17
+legendFontSize <- 8; legendTitleFontSize <- 9
+xlabelFontSize <- 10; ylabelFontSize <- 10
+
 saveEnvironment()
 
